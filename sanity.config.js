3711fa0fb563e6fc675db009e4c27b920c90f5a1
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-05-23 16:27:12 -0400

    Finished Setting Up Staging Environment in Studio

--- sanity.config.js
+++ sanity.config.js
@@ -1,18 +1,38 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
+import {RobotIcon, RocketIcon} from '@sanity/icons'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'blog',
+export default defineConfig([
+  {
+    name: 'production',
+    title: 'Production',
 
-  projectId: 'g014cs9v',
-  dataset: 'production',
+    projectId: 'g014cs9v',
+    dataset: 'production',
+    basePath: '/prodcution',
 
-  plugins: [deskTool(), visionTool()],
+    plugins: [deskTool(), visionTool()],
+    icon: RocketIcon,
 
-  schema: {
-    types: schemaTypes,
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    name: 'staging',
+    title: 'Staging',
+
+    projectId: 'g014cs9v',
+    dataset: 'staging',
+    basePath: '/staging',
+
+    plugins: [deskTool(), visionTool()],
+    icon: RobotIcon,
+
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
--- schemas/posts/personal.js
+++ schemas/posts/personal.js
@@ -35,6 +35,14 @@ export default defineType({
       type: 'datetime',
     }),
     defineField({
+      name: 'thumbnail',
+      title: 'Thumbnail',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
@@ -46,6 +54,19 @@ export default defineType({
       name: 'body',
       title: 'Body',
       type: 'blockContent',
+    }),
+    defineField({
+      title: 'Categories',
+      name: 'categories',
+      type: 'array',
+      of: [
+        {
+          type: 'string',
+        },
+      ],
+      options: {
+        layout: 'tags',
+      },
     }),
     defineField({
       name: 'needsAuthentication',
