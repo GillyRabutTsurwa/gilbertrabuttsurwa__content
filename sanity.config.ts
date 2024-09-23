518500ac4d970efa1660fa0e2baa091b819146a3
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2024-09-23 12:48:43 -0400

    Removing the enableOverlays() With it's Corresponding Package

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,108 +1,107 @@
-import {defineConfig, buildLegacyTheme} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {presentationTool} from 'sanity/presentation'
-import {enableOverlays} from '@sanity/overlays'
-import {EditIcon, DesktopIcon, RobotIcon, RocketIcon, UserIcon} from '@sanity/icons'
-import {codeInput} from '@sanity/code-input'
-import {colorInput} from '@sanity/color-input'
-import {schemaTypes} from './schemas'
-import './custom.css'
+import {defineConfig, buildLegacyTheme} from "sanity";
+import {structureTool} from "sanity/structure";
+import {visionTool} from "@sanity/vision";
+import {presentationTool} from "sanity/presentation";
+import {EditIcon, DesktopIcon, RobotIcon, RocketIcon, UserIcon} from "@sanity/icons";
+import {codeInput} from "@sanity/code-input";
+import {colorInput} from "@sanity/color-input";
+import {schemaTypes} from "./schemas";
+import "./custom.css";
 
-import Logo from './components/Logo'
+import Logo from "./components/Logo";
 
 // try this
 const props = {
-  '--my-blanc': '#fff',
-  '--my-noir': '#1a1a1a',
-  '--my-white': '#4285f4',
-  '--my-black': '#db4437',
-  '--my-blue': '#4285f4',
-  '--my-red': '#db4437',
-  '--my-yellow': '#f4b400',
-  '--my-green': '#0f9d58',
-  '--my-primary': '#1a2934',
-  '--my-secondary': '#510400',
-}
+  "--my-blanc": "#fff",
+  "--my-noir": "#1a1a1a",
+  "--my-white": "#4285f4",
+  "--my-black": "#db4437",
+  "--my-blue": "#4285f4",
+  "--my-red": "#db4437",
+  "--my-yellow": "#f4b400",
+  "--my-green": "#0f9d58",
+  "--my-primary": "#1a2934",
+  "--my-secondary": "#510400",
+};
 
 export const myFirstTheme = buildLegacyTheme({
   /* Base theme colors */
   // '--black': props['--my-black'],
   // '--white': props['--my-white'],
 
-  '--gray': '#666',
-  '--gray-base': '#666',
+  "--gray": "#666",
+  "--gray-base": "#666",
 
-  '--component-bg': props['--my-blanc'],
-  '--component-text-color': props['--my-primary'],
+  "--component-bg": props["--my-blanc"],
+  "--component-text-color": props["--my-primary"],
 
   /* Brand */
-  '--brand-primary': props['--my-primary'], //NOTE: changes background colours of buttons that are active
+  "--brand-primary": props["--my-primary"], //NOTE: changes background colours of buttons that are active
 
   // Default button
-  '--default-button-color': 'pink',
-  '--default-button-primary-color': props['--my-primary'], //NOTE: for the buttons for selecting content types (under content)
-  '--default-button-success-color': props['--my-green'], //NOTE: for (notably) the publish button
-  '--default-button-warning-color': props['--my-yellow'],
-  '--default-button-danger-color': props['--my-red'],
+  "--default-button-color": "pink",
+  "--default-button-primary-color": props["--my-primary"], //NOTE: for the buttons for selecting content types (under content)
+  "--default-button-success-color": props["--my-green"], //NOTE: for (notably) the publish button
+  "--default-button-warning-color": props["--my-yellow"],
+  "--default-button-danger-color": props["--my-red"],
 
   /* State */
-  '--state-info-color': props['--my-blue'],
-  '--state-success-color': props['--my-green'],
-  '--state-warning-color': props['--my-yellow'],
-  '--state-danger-color': props['--my-red'],
+  "--state-info-color": props["--my-blue"],
+  "--state-success-color": props["--my-green"],
+  "--state-warning-color": props["--my-yellow"],
+  "--state-danger-color": props["--my-red"],
 
   /* Navbar */
-  '--main-navigation-color': props['--my-primary'],
-  '--main-navigation-color--inverted': props['--my-blanc'],
-  '--focus-color': props['--my-primary'],
-})
+  "--main-navigation-color": props["--my-primary"],
+  "--main-navigation-color--inverted": props["--my-blanc"],
+  "--focus-color": props["--my-primary"],
+});
 
 export const mySecondTheme = buildLegacyTheme({
   /* Base theme colors */
   // '--black': props['--my-black'],
   // '--white': props['--my-white'],
 
-  '--gray': '#666',
-  '--gray-base': '#666',
+  "--gray": "#666",
+  "--gray-base": "#666",
 
-  '--component-bg': props['--my-blanc'],
-  '--component-text-color': props['--my-secondary'],
+  "--component-bg": props["--my-blanc"],
+  "--component-text-color": props["--my-secondary"],
 
   /* Brand */
-  '--brand-primary': props['--my-secondary'], //NOTE: changes background colours of buttons that are active
+  "--brand-primary": props["--my-secondary"], //NOTE: changes background colours of buttons that are active
 
   // Default button
-  '--default-button-color': 'pink',
-  '--default-button-primary-color': props['--my-secondary'], //NOTE: for the buttons for selecting content types (under content)
-  '--default-button-success-color': props['--my-green'], //NOTE: for (notably) the publish button
-  '--default-button-warning-color': props['--my-yellow'],
-  '--default-button-danger-color': props['--my-red'],
+  "--default-button-color": "pink",
+  "--default-button-primary-color": props["--my-secondary"], //NOTE: for the buttons for selecting content types (under content)
+  "--default-button-success-color": props["--my-green"], //NOTE: for (notably) the publish button
+  "--default-button-warning-color": props["--my-yellow"],
+  "--default-button-danger-color": props["--my-red"],
 
   /* State */
-  '--state-info-color': props['--my-blue'],
-  '--state-success-color': props['--my-green'],
-  '--state-warning-color': props['--my-yellow'],
-  '--state-danger-color': props['--my-red'],
+  "--state-info-color": props["--my-blue"],
+  "--state-success-color": props["--my-green"],
+  "--state-warning-color": props["--my-yellow"],
+  "--state-danger-color": props["--my-red"],
 
   /* Navbar */
-  '--main-navigation-color': props['--my-secondary'],
-  '--main-navigation-color--inverted': props['--my-blanc'],
-  '--focus-color': props['--my-secondary'],
-})
+  "--main-navigation-color": props["--my-secondary"],
+  "--main-navigation-color--inverted": props["--my-blanc"],
+  "--focus-color": props["--my-secondary"],
+});
 
-console.log(codeInput())
+console.log(codeInput());
 
 const SANITY_STUDIO_PREVIEW_URL =
-  process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000/blog'
+  process.env.SANITY_STUDIO_PREVIEW_URL || "http://localhost:3000/blog";
 
 export default defineConfig([
   {
-    name: 'production',
-    title: 'Production',
-    projectId: 'g014cs9v',
-    dataset: 'production',
-    basePath: '/production',
+    name: "production",
+    title: "Production",
+    projectId: "g014cs9v",
+    dataset: "production",
+    basePath: "/production",
 
     plugins: [
       structureTool({
@@ -111,27 +110,27 @@ export default defineConfig([
             .title("Gil's Content")
             .items([
               S.listItem()
-                .title('Posts')
+                .title("Posts")
                 .icon(EditIcon)
                 .child(
                   S.list()
-                    .title('Posts')
+                    .title("Posts")
                     .items([
                       S.listItem()
-                        .title('Personal')
+                        .title("Personal")
                         .icon(UserIcon)
                         .child((categoryId) =>
                           S.documentList()
-                            .title('Personal Posts')
+                            .title("Personal Posts")
                             .filter('_type == "post" && postGenre == "personal"')
                             .params({categoryId}),
                         ),
                       S.listItem()
-                        .title('Tech')
+                        .title("Tech")
                         .icon(DesktopIcon)
                         .child((categoryId) =>
                           S.documentList()
-                            .title('Tech Posts')
+                            .title("Tech Posts")
                             .filter('_type == "post" && postGenre == "tech"')
                             .params({categoryId}),
                         ),
@@ -140,9 +139,9 @@ export default defineConfig([
               // The rest of this document is from the original manual grouping in this series of articles
               //NOTE: ca affiche les autres documents
               ...S.documentTypeListItems().filter(
-                (listItem) => !['post'].includes(listItem.getId()),
+                (listItem) => !["post"].includes(listItem.getId() as string),
               ),
-            ])
+            ]);
         },
       }),
       visionTool(),
@@ -150,9 +149,6 @@ export default defineConfig([
       colorInput(),
       presentationTool({
         previewUrl: SANITY_STUDIO_PREVIEW_URL,
-      }),
-      enableOverlays({
-        allowStudioOrigin: 'http://localhost:3333/production/',
       }),
     ],
     icon: RocketIcon,
@@ -171,11 +167,11 @@ export default defineConfig([
   },
 
   {
-    name: 'staging',
-    title: 'Staging',
-    projectId: 'g014cs9v',
-    dataset: 'staging',
-    basePath: '/staging',
+    name: "staging",
+    title: "Staging",
+    projectId: "g014cs9v",
+    dataset: "staging",
+    basePath: "/staging",
 
     plugins: [
       structureTool({
@@ -184,27 +180,27 @@ export default defineConfig([
             .title("Gil's Content")
             .items([
               S.listItem()
-                .title('Posts')
+                .title("Posts")
                 .icon(EditIcon)
                 .child(
                   S.list()
-                    .title('Posts')
+                    .title("Posts")
                     .items([
                       S.listItem()
-                        .title('Personal')
+                        .title("Personal")
                         .icon(UserIcon)
                         .child((categoryId) =>
                           S.documentList()
-                            .title('Personal Posts')
+                            .title("Personal Posts")
                             .filter('_type == "post" && postGenre == "personal"')
                             .params({categoryId}),
                         ),
                       S.listItem()
-                        .title('Tech')
+                        .title("Tech")
                         .icon(DesktopIcon)
                         .child((categoryId) =>
                           S.documentList()
-                            .title('Tech Posts')
+                            .title("Tech Posts")
                             .filter('_type == "post" && postGenre == "tech"')
                             .params({categoryId}),
                         ),
@@ -213,9 +209,9 @@ export default defineConfig([
               // The rest of this document is from the original manual grouping in this series of articles
               //NOTE: ca affiche les autres documents
               ...S.documentTypeListItems().filter(
-                (listItem) => !['post'].includes(listItem.getId()),
+                (listItem) => !["post"].includes(listItem.getId()),
               ),
-            ])
+            ]);
         },
       }),
       visionTool(),
@@ -223,9 +219,6 @@ export default defineConfig([
       colorInput(),
       presentationTool({
         previewUrl: SANITY_STUDIO_PREVIEW_URL,
-      }),
-      enableOverlays({
-        allowStudioOrigin: 'http://localhost:3333/production/',
       }),
     ],
     icon: RobotIcon,
@@ -242,8 +235,4 @@ export default defineConfig([
 
     theme: mySecondTheme,
   },
-])
-
-// const disable = enableOverlays({
-//   allowStudioOrigin: 'http://localhost:3333/production',
-// })
+]);
