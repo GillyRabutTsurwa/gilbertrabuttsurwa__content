bd06fe61c46311c8b85d81a18ae859bda3d40176
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2024-10-03 18:30:06 -0400

    Fix Misspelling of Portraits Field Name

--- schemas/home/header.ts
+++ schemas/home/header.ts
@@ -24,7 +24,7 @@ export default defineType({
     }),
 
     defineField({
-      name: "portaits",
+      name: "portraits",
       title: "Portrait Images",
       type: "array",
       of: [{ type: "image" }],
